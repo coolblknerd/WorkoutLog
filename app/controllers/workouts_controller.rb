7909ef1f23842c80d45e6598066247ecc2d68276
Reggie Davis <reginald.davis88@gmail.com>
2016-01-31 11:53:14 -0600

    Show/Edit View & Controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -21,10 +21,20 @@ class WorkoutsController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
   def update
+    if @workout.update(workout_params)
+      redirect_to @workout
+    else
+      render 'edit'
+    end
   end
   
   def destroy
+    @workout.destroy
+    redirect_to root_path
   end
   
   private
